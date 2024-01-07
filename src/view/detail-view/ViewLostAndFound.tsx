072f9c6bee833859a67b2461835dedddaf7eadb7
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-01-08 00:05:54 +0530

    Updated lost & found view

--- src/view/detail-view/ViewLostAndFound.tsx
+++ src/view/detail-view/ViewLostAndFound.tsx
@@ -1,5 +1,6 @@
 import LostAndFoundCard from "../../component/LostAndFoundCard.tsx";
 import ComboBox from "../../component/input/combo-box.tsx";
+import CustomButton from "../../component/input/custom-button.tsx";
 
 interface LostAndFoundItem {
     title: string;
@@ -11,7 +12,7 @@ interface LostAndFoundItem {
 }
 
 function ViewLostAndFound({list}: { list: LostAndFoundItem[] }) {
-    return (<div className={'w-4/5 py-10 px-14 m-auto border-1 border-emerald-400 rounded-2xl mt-6'}>
+    return (<div className={'w-4/5 py-10 px-14 m-auto border-1 border-emerald-400 rounded-2xl my-6'}>
 
         {/*center a paragraph*/}
         <div className={'flex flex-row justify-center items-center'}>
@@ -28,6 +29,15 @@ function ViewLostAndFound({list}: { list: LostAndFoundItem[] }) {
         <div className="gap-12 grid grid-cols-2 sm:grid-cols-4 mt-5">
             {list.map((item: LostAndFoundItem, index: number) => (<LostAndFoundCard key={index} item={item}/>))}
         </div>
+
+        {/*add 'more' button using custom-button.tsx*/}
+        <div className={'flex flex-row justify-end items-center pt-12 gap-4'}>
+            {/*add another custom button here*/}
+            <CustomButton borderColor={'#59AE4B'} bgColor={'white'} hoverColor={'#59AE4B'} textColor={'#59AE4B'} textHoverColor={'white'} text={'Notice'}/>
+            <CustomButton borderColor={'#071722'} bgColor={'white'} hoverColor={'#071722'}
+                          textColor={'#071722'} textHoverColor={'white'} text={'More'}/>
+        </div>
+
     </div>);
 }
 
